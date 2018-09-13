3af0e25c3f58ed6c39b1f97f3c84edae77947977
Manan Jobanputra <manan.jobanputra@gmail.com>
2018-09-13 18:39:34 +0530

    Added static design of login, register & Dashboard page

--- src/components/Layout/Layout.jsx
+++ src/components/Layout/Layout.jsx
@@ -1,22 +1,39 @@
 // External libraries
 import React, { Component } from "react";
+import {Switch, Route } from "react-router-dom";
 
 // Components
-import Main from "./Main/Main";
-import Header from "./Header/Header";
+import Basepages from './Basepages';
+import Base from './Base';
+import Login from '../Authentication/Login/Login';
+import Register from '../Authentication/Register/Register';
+import Dashboard from '../Dashboard/Dashboard';
 
-// Styling
-import "./Layout.css";
+import {withRouter} from 'react-router-dom';
 
 class Layout extends Component {
+  
     render() {
-        return (
-            <div className="Layout">
-                <Header />
-                <Main />
-            </div>
-        );
+        const location = this.props.location.pathname.substr(1);
+        if(location ==='login' || location ==='register'){
+              return (
+                <Basepages>
+                    <Route  path="/" exact={true} component={Login} />
+                    <Route  path="/login" exact={true} component={Login} />
+                    <Route  path="/register" component={Register} />
+                </Basepages>
+            );
+        }
+        else{
+            return (
+                <Base>
+                    <Switch>
+                        <Route path="/dashboard" component={Dashboard}/> 
+                    </Switch>
+                </Base>
+            )
+        }
     }
 }
 
-export default Layout;
+export default withRouter(Layout);
---
+++ src/components/Layout/Sidebar.jsx
@@ -0,0 +1,164 @@
+import React,{Component} from 'react';
+import { Link } from 'react-router-dom';
+import ScrollArea  from 'react-scrollbar';
+import './Header';
+class Sidebar extends Component{
+   constructor(props)
+   {
+     super(props);
+    
+     this.dashboard=this.dashboard.bind(this);
+     this.elements=this.elements.bind(this);
+     this.calendarmenu=this.calendarmenu.bind(this);
+     this.form=this.form.bind(this);
+     this.sidebarnav=this.sidebarnav.bind(this);
+     this.table=this.table.bind(this);
+     this.custompage=this.custompage.bind(this);
+     this.authentication=this.authentication.bind(this);
+     this.multilevel=this.multilevel.bind(this);
+     this.auth=this.auth.bind(this);
+     this.login=this.login.bind(this);
+     this.invoice=this.invoice.bind(this);
+     this.error=this.error.bind(this);
+     
+     this.state={
+       dashboard:false,
+       elements:false,
+       calendarmenu:false,
+       form:false,
+       sidebarnav:false,
+       table:false,
+       custompage:false,
+       authentication:false,
+       multilevel:false,
+       auth:false,
+       login:false,
+       invoice:false,
+       error:false,
+       plussignele:false,
+       plussignform:false,
+       plussigndata:false,
+       plussigncustome:false,
+       plussignauthentic:false,
+       plussignmulti:false,
+       plussignauth:false,
+       plussignlogin:false,
+       plussigninvo:false,
+       plussignerror:false
+     }
+   }
+
+   dashboard()
+   {
+      this.setState({
+        dashboard:!this.state.dashboard
+      })
+   }
+   elements()
+   {
+     this.setState({
+       elements:!this.state.elements,
+       plussignele:!this.state.plussignele
+     })
+   }
+   calendarmenu()
+   {
+     this.setState({
+        calendarmenu:!this.state.calendarmenu
+     })
+   }
+   sidebarnav()
+   {
+     this.setState({
+      sidebarnav:!this.state.sidebarnav
+     })
+   }
+   form()
+   {
+     this.setState({
+      form:!this.state.form,
+      plussignform:!this.state.plussignform
+     })
+   }
+   table()
+   {
+     this.setState({
+       table:!this.state.table,
+       plussigndata:!this.state.plussigndata
+     })
+   }
+   custompage(){
+     this.setState({
+       custompage:!this.state.custompage,
+       plussigncustome:!this.state.plussigncustome
+     })
+   }
+   authentication(){
+     this.setState({
+       authentication:!this.state.authentication,
+       plussignauthentic:!this.state.plussignauthentic
+     })
+   }
+   multilevel(){
+     this.setState({
+      multilevel:!this.state.multilevel,
+      plussignmulti:!this.state.plussignmulti
+     })
+   }
+
+   auth(){
+    this.setState({
+      auth:!this.state.auth,
+      plussignauth:!this.state.plussignauth
+    })
+  }
+  login(){
+    this.setState({
+      login:!this.state.login,
+      plussignlogin:!this.state.plussignlogin
+    })
+  }
+  invoice(){
+    this.setState({
+      invoice:!this.state.invoice,
+      plussigninvo:!this.state.plussigninvo
+    })
+  }
+  error(){
+    this.setState({
+      error:!this.state.error,
+      plussignerror:!this.state.plussignerror
+    })
+  }
+ render(){
+        return(
+           // <!-- Left Sidebar start-->
+          
+          //  <Collapse isOpen={this.props.toggerbutton}>
+            <div className="side-menu-fixed">
+            <ScrollArea speed={0.8} style={{overflow: 'hidden'}}
+                                    className="scrollbar side-menu-bg"
+                                    contentClassName="saidbar"
+                                    horizontal={false} > 
+              <div className="saidbar"> 
+            
+              <ul className="nav navbar-nav side-menu" id="sidebarnav">
+                {/* <!-- menu item Dashboard--> */}
+                <li className="active">
+                  <Link to="/dashboard"><i className="ti-home"></i><span className="right-nav-text">Dashboard</span></Link>  
+                </li>
+                <li className="active">
+                  <Link to="/dashboard"><i className="ti-home"></i><span className="right-nav-text">Market</span></Link>  
+                </li>
+                <li className="active">
+                  <Link to="/dashboard"><i className="ti-home"></i><span className="right-nav-text">My Wallet</span></Link>  
+                </li>
+              </ul>
+            </div>
+          </ScrollArea> 
+        </div> 
+        // </Collapse>
+        );
+    }
+}
+export default Sidebar;
