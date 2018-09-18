7ed7245c45cb7f9ac005cede9566f01814cc34e4
Carlo Pascoli <carlo.pascoli@gmail.com>
2018-09-18 09:07:54 +0100

    new endpoints

--- server.js
+++ server.js
@@ -8,9 +8,16 @@ var getProxyRules = new HttpProxyRules({
   rules: {
     '/api/ping': 'http://localhost:3000/ping',
     '/api/loanRequests/([0-9]+)': 'http://localhost:3000/loanrequest/$1', 
-    '/api/loanRequests/*': 'http://localhost:3000/loanrequest/all', 
+    '/api/user/loanRequests': 'http://localhost:3000/user/loanrequests',    
+    '/api/loanRequests/*': 'http://localhost:3000/loanrequest/all',
     '/api/relayerFee': 'http://localhost:3000/config/relayerFee',
-    '/api/relayerAddress': 'http://localhost:3000/config/relayerAddress'
+    '/api/relayerAddress': 'http://localhost:3000/config/relayerAddress',
+
+    '/api/swagger-ui.html': 'http://localhost:3000/swagger-ui.html',
+    '/api/webjars/(.+)': 'http://localhost:3000/webjars/$1',
+    '/api/swagger-resources(.*)': 'http://localhost:3000/swagger-resources/$1',
+    '/api/v2/(.+)': 'http://localhost:3000/v2/$1'
+
   }
 });
 
