a6f70a7bdf86dcd7099c3b4365af25d97477e39a
Manan Jobanputra <manan.jobanputra@gmail.com>
2018-09-25 20:42:45 +0530

    Added Google Login

--- src/components/Authentication/Login/Login.jsx
+++ src/components/Authentication/Login/Login.jsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 import { Container, Row, Col } from 'reactstrap';
 import { toast } from 'react-toastify';
 import validators from '../../../validators';
+import GoogleLogin from "react-google-login";
 import Api from "../../../services/api";
 import './Login.css';
 class Login extends React.Component {
@@ -13,17 +14,37 @@ class Login extends React.Component {
       password: '',
       error: null,
     };
+    this.signup = this.signup.bind(this);
     this.validators = validators;
     this.onchange = this.onchange.bind(this);
     this.login = this.login.bind(this);
 
     this.displayValidationErrors = this.displayValidationErrors.bind(this);
     this.updateValidators = this.updateValidators.bind(this);
-    var user_authorization_token = localStorage.getItem('user_authorization_token');
-    if (user_authorization_token != '' && user_authorization_token != null) {
-      this.props.history.push("/dashboard");
-    }
   }
+
+  async googleSignIn(token) {
+    const api = new Api();
+    return new Promise((resolve) => {
+      api.create("goauthlogin", { token: token }).then((response) => {
+        resolve(response);
+      });
+    });
+  }
+
+  signup(res, type) {
+    // console.log("have google profile: ", res.w3.U3);
+    console.log("tokenId: ", res.tokenId);
+    this.googleSignIn(res.tokenId).then(response => {
+      console.log("logged-in as '", response.name, "' email: ", response.email);
+      this.setState({
+        name: response.name,
+        email: response.email,
+        pictureUrl: response.pictureUrl
+      })
+    });
+  }
+
   onchange(event) {
     this.setState({
       [event.target.name]: event.target.value
@@ -85,25 +106,35 @@ class Login extends React.Component {
       username: email,
       password: password
     });
+    console.log(response)
     //const authorization = response.headers.get('Authorization');
     const authorization = response.headers.get('Authorization');
     console.log(authorization)
-    if (authorization != null) {
+    if (authorization && authorization != null) {
       localStorage.setItem('token', authorization);
-      this.props.history.push("/dashboard");
+      this.props.history.push("/");
     }
     else {
       toast.error("Failed");
     }
   }
   render() {
+
+    const responseGoogle = response => {
+      console.log("google console");
+      console.log(response);
+      this.signup(response, "google");
+    };
+
     return (
       <section className="height-100vh d-flex align-items-center page-section-ptb login" style={{ backgroundImage: 'url(assets/images/login-bg.png)' }}>
         <Container>
           <Row className="justify-content-center no-gutters vertical-align">
             <Col lg={4} md={6} className="login-fancy-bg bg" style={{ backgroundImage: 'url(assets/images/login-inner-bg.png)' }}>
               <div className="login-fancy">
-                <h2 className="text-white mb-20 text-center"><img src="assets/images/logo-full.svg" alt="Cognito Chain" width="200" /></h2>
+                <h2 className="text-white mb-20 text-center">
+                  <a href="/"><img src="assets/images/logo-full.svg" alt="Cognito Chain" width="200" /></a>
+                </h2>
                 <p className="mb-20 text-white">Create tailor-cut websites with the exclusive multi-purpose responsive template along with powerful features.</p>
                 <ul className="list-unstyled  pos-bot pb-30">
                   <li className="list-inline-item"><a className="text-white" href="#"> Terms of Use</a> </li>
@@ -131,10 +162,25 @@ class Login extends React.Component {
 
                   </div>
                 </div>
-                <a onClick={this.login} className={`button   ${this.isFormValid() ? '' : 'disabled'}`}>
-                  <span className="text-white">Log in</span>
-                  <i className="fa fa-check text-white" />
-                </a>
+
+                <div>
+                  <a onClick={this.login} className={`button   ${this.isFormValid() ? '' : 'disabled'}`}>
+                    <span className="text-white">Log in</span>
+                    <i className="fa fa-check text-white" />
+                  </a>
+
+                  <span className="login-buttons-seperator">OR</span>
+
+                  <GoogleLogin
+                    clientId="166486140124-jglmk5i5fu0bvk6fh8q2hl25351pfst0.apps.googleusercontent.com"
+                    buttonText="Login with Google"
+                    onSuccess={responseGoogle}
+                    onFailure={responseGoogle}
+                    className="google-login-btn"
+                  />
+
+                </div>
+
                 <p className="mt-20 mb-0 remember-checkbox">Don't have an account? <Link to="/register" > Create one here </Link></p>
               </div>
             </Col>
