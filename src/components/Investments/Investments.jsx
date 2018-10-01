19638bb02327aefa67267107caddfab5de6cb846
Manan Jobanputra <manan.jobanputra@gmail.com>
2018-10-01 15:05:11 +0530

    Invest page design update

--- src/components/Investments/Investments.jsx
+++ src/components/Investments/Investments.jsx
@@ -1,5 +1,5 @@
 // External libraries
-import Dharma from "@dharmaprotocol/dharma.js";
+import { Dharma } from "@dharmaprotocol/dharma.js";
 import React from "react";
 import BootstrapTable from "react-bootstrap-table-next";
 
@@ -9,30 +9,30 @@ import Title from "../Title/Title";
 import Loading from "../Loading/Loading";
 
 const columns = [
-    {
-        dataField: "principal",
-        text: "Principal",
-    },
-    {
-        dataField: "interestRate",
-        text: "Interest Rate",
-    },
-    {
-        dataField: "term",
-        text: "Term Length",
-    },
-    {
-        dataField: "collateral",
-        text: "Collateral",
-    },
-    {
-        dataField: "repaidAmount",
-        text: "Repaid",
-    },
-    {
-        dataField: "totalExpectedRepaymentAmount",
-        text: "Total Expected Repayment",
-    },
+{
+    dataField: "principal",
+    text: "Principal",
+},
+{
+    dataField: "interestRate",
+    text: "Interest Rate",
+},
+{
+    dataField: "term",
+    text: "Term Length",
+},
+{
+    dataField: "collateral",
+    text: "Collateral",
+},
+{
+    dataField: "repaidAmount",
+    text: "Repaid",
+},
+{
+    dataField: "totalExpectedRepaymentAmount",
+    text: "Total Expected Repayment",
+},
 ];
 
 class Investments extends React.Component {
@@ -48,16 +48,29 @@ class Investments extends React.Component {
 
     async componentDidMount() {
         const { dharma } = this.props;
-
-        const { Investments } = Dharma.Types;
-
+        const { Investments, Investment, Debt } = Dharma.Types;
         const creditor = await dharma.blockchain.getCurrentAccount();
 
         const investments = await Investments.getExpandedData(dharma, creditor);
+        console.log("-- investments --");
+        console.log(investments);
+
+        // Test loading a Debt and an Investment by id (agreementId)
+        let agreementId = "0xf509b73911c77950846c794c770d562ac3d3411ce99750bb5c5da48c68a0ce43";
+        
+        console.log("-- investment --");
+        const investment = await Investment.fetch(dharma, agreementId);
+        console.log(investment);
+
+        console.log("-- debt --");
+        const debt = await Debt.fetch(dharma, agreementId);
+        console.log(debt);
 
         this.setState({
             investments,
         });
+
+
     }
 
     getData() {
@@ -90,15 +103,15 @@ class Investments extends React.Component {
 
         return (
             <div className="Investments">
-                <Title>Your Investments</Title>
+            <Title>Your Investments</Title>
 
-                {
-                    data
-                        ? <BootstrapTable keyField="id" columns={columns} data={data} />
-                        : <Loading/>
-                }
+            {
+                data
+                ? <BootstrapTable keyField="id" columns={columns} data={data} />
+                : <Loading/>
+            }
             </div>
-        );
+            );
     }
 }
 
