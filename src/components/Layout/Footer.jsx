abab77a08ab44fd5e98880cb8bc30339a8d01022
Manan Jobanputra <manan.jobanputra@gmail.com>
2018-10-29 22:01:31 +0530

    Removed API policy link

--- src/components/Layout/Footer.jsx
+++ src/components/Layout/Footer.jsx
@@ -16,7 +16,6 @@ class Footer extends Component{
                     <Col md={6}>
                       <ul className="text-center text-md-right">
                         <li className="list-inline-item"><a href="terms" target="_blank">Terms &amp; Conditions | </a> </li>
-                        <li className="list-inline-item"><a href="#">API Use Policy | </a> </li>
                         <li className="list-inline-item"><a href="privacy" target="_blank">Privacy Policy </a> </li>
                       </ul>
                     </Col>
