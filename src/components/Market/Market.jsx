98b0098898b829ef1c29495fc36b1e704f4d3309
Manan Jobanputra <manan.jobanputra@gmail.com>
2018-11-12 17:38:58 +0530

    formatting code

--- src/components/Market/Market.jsx
+++ src/components/Market/Market.jsx
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Card,CardBody,CardTitle,Row,Col } from 'reactstrap';
+import { Card, CardBody, CardTitle, Row, Col } from 'reactstrap';
 import { Link } from 'react-router-dom';
 import DharmaConsumer from "../../contexts/Dharma/DharmaConsumer";
 import LoanRequests from "./LoanRequests/LoanRequests";
@@ -12,7 +12,7 @@ class Market extends Component {
         this.redirect = this.redirect.bind(this);
         this.parseQueryParams = this.parseQueryParams.bind(this);
     }
-    
+
     redirect(location) {
         this.props.history.push(location);
     }
@@ -39,60 +39,59 @@ class Market extends Component {
 
         return (
             <div className="content-with-market-bg">
-                    <div className="market-bg-image">
-                            <div className="image" style={{backgroundImage:"url('assets/images/loans.jpg')"}}></div>
-                            <div className="bg-overlay"></div>
-                            <div className="market-bg-content">
-                                <h5>Digital assets backed loans got much easier. Here is how</h5>
-                                <div className="row mt-30 how-it-works">
-                                    <div className="col-md-3 p-2">
-                                        <div className="step-container p-2">
-                                            <h5 className="text-muted">Step 1</h5>
-                                            <p>Login to Cognitochain Marketplace</p>
-                                            <h6>Create Loan Request</h6>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-3 p-2">
-                                        <div className="step-container p-2">
-                                            <h5 className="text-muted">Step 2</h5>
-                                            <p>Authorize Smart Contract</p>
-                                            <h6>Unlock Collateral</h6>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-3 p-2">
-                                        <div className="step-container p-2">
-                                            <h5 className="text-muted">Step 3</h5>
-                                            <p>Publish in Cognitochain Marketplace</p>
-                                            <h6>Get Funded</h6>
-                                        </div>
-                                    </div>
-                                    <div className="col-md-3 p-2">
-                                        <div className="step-container p-2">
-                                            <h5 className="text-muted">Step 4</h5>
-                                            <p>Repay Loan</p>
-                                            <h6>Get Collateral back in full</h6>
-                                        </div>
-                                    </div>
+                <div className="market-bg-image">
+                    <div className="image" style={{ backgroundImage: "url('assets/images/loans.jpg')" }}></div>
+                    <div className="bg-overlay"></div>
+                    <div className="market-bg-content">
+                        <h5>Digital assets backed loans got much easier. Here is how</h5>
+                        <div className="row mt-30 how-it-works">
+                            <div className="col-md-3 p-2">
+                                <div className="step-container p-2">
+                                    <h5 className="text-muted">Step 1</h5>
+                                    <p>Login to Cognitochain Marketplace</p>
+                                    <h6>Create Loan Request</h6>
                                 </div>
                             </div>
+                            <div className="col-md-3 p-2">
+                                <div className="step-container p-2">
+                                    <h5 className="text-muted">Step 2</h5>
+                                    <p>Authorize Smart Contract</p>
+                                    <h6>Unlock Collateral</h6>
+                                </div>
+                            </div>
+                            <div className="col-md-3 p-2">
+                                <div className="step-container p-2">
+                                    <h5 className="text-muted">Step 3</h5>
+                                    <p>Publish in Cognitochain Marketplace</p>
+                                    <h6>Get Funded</h6>
+                                </div>
+                            </div>
+                            <div className="col-md-3 p-2">
+                                <div className="step-container p-2">
+                                    <h5 className="text-muted">Step 4</h5>
+                                    <p>Repay Loan</p>
+                                    <h6>Get Collateral back in full</h6>
+                                </div>
+                            </div>
+                        </div>
                     </div>
-                    <div className="page-title">
-                        <Row>
-                            <Col sm={6}>
-                                <h4 className="mb-0"> Market</h4>
-                                <div className='delete-button' onClick={(item) => { if (window.confirm('Are you sure you wish to delete this item?')) this.onCancel(item) }} />
-                            </Col>
-                            <Col sm={6} className="pull-right text-right">
-                                <Link to="/create"><span className="btn cognito orange small icon mb-15 btn-market-borrow"><img src="assets/images/borrow.png" height="20" alt="Borrow" /> Borrow</span></Link>
-                            </Col>
-                        </Row>
-                    </div>
-                    {/* <!-- widgets --> */}
-                    <Row className="open-request-table">
-                            
-                        <Col md={12} className="mb-30">
+                </div>
+                <div className="page-title">
+                    <Row>
+                        <Col sm={6}>
+                            <h4 className="mb-0"> Market</h4>
+                            <div className='delete-button' onClick={(item) => { if (window.confirm('Are you sure you wish to delete this item?')) this.onCancel(item) }} />
+                        </Col>
+                        <Col sm={6} className="pull-right text-right">
+                            <Link to="/create"><span className="btn cognito orange small icon mb-15 btn-market-borrow"><img src="assets/images/borrow.png" height="20" alt="Borrow" /> Borrow</span></Link>
+                        </Col>
+                    </Row>
+                </div>
+                {/* <!-- widgets --> */}
+                <Row className="open-request-table">
+
+                    <Col md={12} className="mb-30">
                         <Card className="card-statistics h-100 p-3">
-                            
                             <CardBody>
                                 <div className="d-block d-md-flex justify-content-between" style={{ position: "relative" }}>
                                     <div className="d-block w-100">
@@ -113,30 +112,25 @@ class Market extends Component {
                             </CardBody>
                         </Card>
                     </Col>
+                </Row>
 
-                    </Row>
-
-
-                    <Row className="recent-funded-loans-table">
-                            
-                        <Col md={12} className="mb-30">
+                <Row className="recent-funded-loans-table">
+                    <Col md={12} className="mb-30">
                         <Card className="card-statistics h-100 p-3">
                             <CardBody>
                                 <CardTitle>Recent Funded Loans</CardTitle>
                                 <DharmaConsumer>
-                                    { (dharmaProps) => {
-                                        return <FundedLoans 
-                                        dharma={ dharmaProps.dharma }
-                                        redirect={this.redirect}
+                                    {(dharmaProps) => {
+                                        return <FundedLoans
+                                            dharma={dharmaProps.dharma}
+                                            redirect={this.redirect}
                                         />
-                                    } }
+                                    }}
                                 </DharmaConsumer>
                             </CardBody>
                         </Card>
                     </Col>
-
-                    </Row>
-                
+                </Row>
             </div>
 
         );
