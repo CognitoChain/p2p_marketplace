b91e71a81e1871c7bcc317137a5c86a846c50fcc
Sanket Jayani <sanket.jayani@gmail.com>
2018-11-17 07:22:50 +0530

    Added big nubmer conversation function in global file to access everywhere

--- src/utils/Util.jsx
+++ src/utils/Util.jsx
@@ -1,3 +1,6 @@
+import _ from 'lodash';
+import { BigNumber } from "bignumber.js";
+
 export const amortizationUnitToFrequency = (unit) => {
     let frequency = "";
     switch (unit) {
@@ -27,3 +30,16 @@ export const niceNumberDisplay = (value) => {
     niceNumber = (niceNumber > 0) ? niceNumber.toFixed(3) : 0;
     return niceNumber;
 }
+export const convertBigNumber= (obj,power) => {
+    let responseNumber = 0;
+    if(_.isObject(obj)){
+      responseNumber = obj.div(new BigNumber(10).pow(power.toNumber()));  
+    }
+    else{
+      let decimal = "1E" + power;
+      let amount = obj / decimal;
+      responseNumber = amount; 
+    }
+    responseNumber = (responseNumber > 0) ? parseFloat(responseNumber) : 0;
+    return responseNumber;
+  }
