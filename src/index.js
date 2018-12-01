fcd72d9ee99e5fc315907b510ae2307664875844
Carlo Pascoli <carlo.pascoli@gmail.com>
2018-12-01 13:26:55 +0000

    google analytics

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter } from "react-router-dom";
-
+import ReactGA from 'react-ga';
+import createHistory from 'history/createBrowserHistory';
 import registerServiceWorker from "./registerServiceWorker";
 
 import App from "./App";
@@ -10,6 +11,28 @@ import App from "./App";
 import DharmaProvider from "./contexts/Dharma/DharmaProvider";
 
 import './styles.scss';   
+import './styles.scss';   
+
+//  Google Analytics
+let gatid = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+let history = null;
+
+if (gatid === undefined) {
+    console.log("no google analytics");
+} else {
+
+    console.log("gatid: ", gatid);
+    ReactGA.initialize(gatid);
+    history = createHistory();
+    console.log("path: ", history.location.pathname);
+    ReactGA.pageview(history.location.pathname);
+    
+    // track pageviews when history changes
+    const unlisten = history.listen((location, action) => {
+        console.log("path: ", action, location.pathname, location.state);
+        ReactGA.pageview(location.pathname);
+    });
+}
 
 
 ReactDOM.render(
